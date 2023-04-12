0e10ced379faf5e426aa7d59d805299e95831ee7
manoj <manojpoojary2004@gmail.com>
2023-04-12 16:53:21 +0530

    changed in page

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 var out=document.querySelector('.out')
-var story=document.querySelector('.story')
+var story=document.querySelector('.s')
 var mj="https://shortstories-api.onrender.com/"
+
 story.addEventListener("click",function op(){
     fetch(mj)
     .then(function fun(e){
      return e.json();
-    
     })
     .then(function f(p){
     //    out.value= p.story
        localStorage.setItem("story",p.story);
        location.replace("story.html")
     })
+    
 })
 
+//Arrow function
+
+// function Asum(a,b){
+//     return a+b;
+// }
+
+// let sum=(a,b)=>a+b;
+// let a=sum(10,20);
+// console.log(a);
